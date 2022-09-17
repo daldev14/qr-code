ac0377e7ccc669d9427bda87308d754f4692ca37
Daniel Amaya <damayalop@gmail.com>
2022-09-16 18:14:13 -0600

    First commit: html, css and js

---
+++ src/generate-qr.js
@@ -0,0 +1,18 @@
+const containerQR = document.getElementById('containerQR')
+const formQR = document.getElementById('formQR')
+formQR.inputLink.value = ""
+
+const QR = new QRCode(containerQR, {
+  text: 'https://github.com/Amaya-14',
+  width: 170,
+  height: 170,
+  colorDark: "#ffffff",
+  colorLight: "#ffffff00",
+  correctLevel: QRCode.CorrectLevel.H
+})
+
+formQR.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+  QR.makeCode(formQR.inputLink.value)
+  formQR.inputLink.value = ""
+})
